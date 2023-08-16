54138a3bec14ce3d1e1d5cac01684e3a3ca75731
nonfermata <nonfermata@gmail.com>
2023-08-16 18:36:50 +0600

    'Dima'

--- src/index.js
+++ src/index.js
@@ -2,16 +2,17 @@ import '../index.css';
 import img1 from '../assets/img1.png';
 
 const main = document.querySelector('main');
+console.log('Dima');
 const jsImage = document.createElement('img');
 jsImage.className = 'js-image';
 jsImage.src = img1;
 main.append(jsImage);
 
-console.log(isPalindrome('околомишимолоко'))
-console.log(isPalindrome('kabak'))
+console.log(isPalindrome('околомишимолоко'));
+console.log(isPalindrome('kabak'));
 
 function isPalindrome(str) {
   return str === str.split('').reverse().join('');
 }
 
-console.log('Что-то пошло ТАК')
+console.log('Что-то пошло ТАК');
